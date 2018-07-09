788f9f4d85808a87f977a57ab42c09362c0f0960
Gerald Versluis <gerald@verslu.is>
2018-07-09 09:53:04 +0200

    Initial commit

---
+++ BlurryZXing/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using ZXing.Common;
+
+namespace BlurryZXing
+{
+	public partial class MainPage : ContentPage
+	{
+		public EncodingOptions BarcodeOptions => new EncodingOptions() { Height = 300, Width = 300, PureBarcode = true };
+
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
